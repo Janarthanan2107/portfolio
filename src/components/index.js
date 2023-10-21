d2e2a9bcbdf0d12b4a49485949c3270c4a889f51
Janarthanan2107 <janarthanan.v2107@gmail.com>
2023-10-21 19:32:32 +0530

    importing in index

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,9 @@
+// nav and foot
 import Navbar from "./navbar/navbar.Component";
 import Footer from "./footer/footer.Component";
 
-export { Navbar, Footer }
+// main container
+import Hero from "./hero/hero.Component";
+import Skills from "./skills/skills.Component";
+
+export { Navbar, Footer, Hero, Skills }
