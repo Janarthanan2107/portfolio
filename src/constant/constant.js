5d7e40cb0760c2413339e38758497e3e32bfbdfd
Janarthanan2107 <janarthanan.v2107@gmail.com>
2023-10-21 19:29:16 +0530

    constant datas

---
+++ src/constant/constant.js
@@ -0,0 +1,29 @@
+const skillsData = [
+    {
+        id: 1,
+        img: "/assets/skills-png/frontendDeveloper.png",
+        skill: "Frontend",
+        desc: "Experienced frontend developer with a keen eye for design and a commitment to crafting engaging, user-friendly web interfaces that captivate and delight users.",
+    },
+    {
+        id: 2,
+        img: "/assets/skills-png/backendDevelopment.png",
+        skill: "Backend",
+        desc: "Backend specialist with a focus on robust server-side development, ensuring data integrity and optimal performance for web applications. Proficient in database management and API integration.",
+    },
+    {
+        id: 3,
+        img: "/assets/skills-png/uiUx.png",
+        skill: "UI/UX",
+        desc: "Passionate UI/UX designer dedicated to creating visually stunning and highly intuitive user interfaces that elevate the digital experience. Adept at crafting designs that seamlessly blend aesthetics with usability.",
+    },
+    {
+        id: 4,
+        img: "/assets/skills-png/webDevelopment.png",
+        skill: "Web Development",
+        desc: "Skilled web developer adept at building and optimizing dynamic, user-friendly websites and applications. Proficient in both front-end and back-end technologies to deliver seamless online experiences.",
+    },
+];
+
+
+export { skillsData }
