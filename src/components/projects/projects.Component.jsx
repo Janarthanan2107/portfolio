b703dd53a07a9f49b3705e214710ac4e343b6d8f
Janarthanan2107 <janarthanan.v2107@gmail.com>
2023-10-21 21:59:48 +0530

    mapped live to btn

--- src/components/projects/projects.Component.jsx
+++ src/components/projects/projects.Component.jsx
@@ -1,5 +1,5 @@
 import { projectsData } from "../../constant/constant";
-
+import "./project.css";
 const Projects = () => {
   return (
     <div className="projects">
@@ -14,21 +14,35 @@ const Projects = () => {
             </p>
           </div>
           <div>
-            <button className="py-3 px-4 font-semibold border border-primary rounded-md bg-primary text-white hover:bg-white hover:text-primary transition duration-250 ease-out hover:ease-in">
-              Visit My Github
-            </button>
+            <a href="https://github.com/Janarthanan2107">
+              <button className="py-3 px-4 font-semibold border border-primary rounded-md bg-primary text-white hover:bg-white hover:text-primary transition duration-250 ease-out hover:ease-in">
+                <i class="fa-brands fa-github mr-2"></i>Visit My Github
+              </button>
+            </a>
           </div>
         </div>
         <div className="mt-5 flex justify-center items-center">
           <div className="skill-container grid grid-cols-3 items-center justify-center gap-8">
             {projectsData.map((item) => {
-              const { id, img, title, desc, link } = item;
+              const { id, img, title, desc, live, link } = item;
               return (
                 <div
                   className="card bg-white rounded-md hover:shadow-lg"
                   key={id}
                 >
-                  <img src={img} alt="project-img" className="rounded-md" />
+                  <div class="project-img-container">
+                    <div class="project-img">
+                      <img src={img} alt="project-img" className="rounded-md" />
+                      <div class="details">
+                        <a href={live}>
+                          <button className="py-2 px-4 font-semibold border border-primary rounded-md bg-primary text-white hover:bg-white hover:text-primary transition duration-250 ease-out hover:ease-in">
+                            Project live
+                            <i class="fa-solid fa-arrow-right -rotate-45 mx-1"></i>
+                          </button>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
                   <div className="p-3 flex flex-col gap-4">
                     <p className="text-[1.5rem] font-semibold">{title}</p>
                     <p className="line-clamp-4 text-gray-500">{desc}</p>
