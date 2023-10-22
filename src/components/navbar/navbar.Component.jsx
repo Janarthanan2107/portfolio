1be251b7c1276721041bcc425c696a17cde28afd
Janarthanan2107 <janarthanan.v2107@gmail.com>
2023-10-23 00:20:56 +0530

    setting up active state

--- src/components/navbar/navbar.Component.jsx
+++ src/components/navbar/navbar.Component.jsx
@@ -1,9 +1,11 @@
 // import context
+import { useState } from "react";
 import { useDarkTheme } from "../../context/theme.Context";
 
-const Navbar = () => {
+const Navbar = ({ smoothScrollTo, activeSection }) => {
   // use our theme context here!
   const { theme } = useDarkTheme();
+
   return (
     <div
       className={`navbar fixed top-0 left-0 w-full shadow-md ${
@@ -11,36 +13,58 @@ const Navbar = () => {
       } z-10 transition duration-500 ease-out hover:ease-in`}
     >
       <div className="flex justify-between items-center m-5">
-        <div className={`logo font-poppins text-[1.5rem] tracking-wide font-bold ${theme ? "text-gray-300":"text-gray-600"} `}>
+        <div
+          className={`logo font-poppins text-[1.5rem] tracking-wide font-bold ${
+            theme ? "text-gray-300" : "text-gray-600"
+          } `}
+        >
           <i className="fa-solid fa-code text-blue-500"></i> Janarthanan.
         </div>
         <div className="menu">
           <ul className="flex gap-7 text-[1.1150rem] font-medium">
             <li
               className={`cursor-pointer hover:text-primary hover:-translate-y-[5px] ${
-                theme ? "text-white" : "text-black"
+                activeSection === "home"
+                  ? "text-primary underline underline-offset-8"
+                  : theme
+                  ? "text-white"
+                  : "text-black"
               }`}
+              onClick={() => smoothScrollTo("home")}
             >
               Home
             </li>
             <li
               className={`cursor-pointer hover:text-primary hover:-translate-y-[5px] ${
-                theme ? "text-white" : "text-black"
+                activeSection === "skills"
+                  ? "text-primary underline underline-offset-8"
+                  : theme
+                  ? "text-white"
+                  : "text-black"
               }`}
+              onClick={() => smoothScrollTo("skills")}
             >
               Skills
             </li>
             <li
               className={`cursor-pointer hover:text-primary hover:-translate-y-[5px] ${
-                theme ? "text-white" : "text-black"
+                activeSection === "portfolio"
+                  ? "text-primary underline underline-offset-8"
+                  : theme
+                  ? "text-white"
+                  : "text-black"
               }`}
+              onClick={() => smoothScrollTo("portfolio")}
             >
               Portfolio
             </li>
           </ul>
         </div>
         <div className="contact">
-          <button className="py-3 px-3 font-semibold border border-primary rounded-md text-primary hover:bg-primary hover:text-white transition duration-250 ease-out hover:ease-in">
+          <button
+            className="py-3 px-3 font-semibold border border-primary rounded-md text-primary hover:bg-primary hover:text-white transition duration-250 ease-out hover:ease-in"
+            onClick={() => smoothScrollTo("form")}
+          >
             Contact Me
           </button>
         </div>
