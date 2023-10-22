bd2cf06a8af0b6d6f32cbf892782d4bd6fdf509e
Janarthanan2107 <janarthanan.v2107@gmail.com>
2023-10-22 21:48:24 +0530

    setting context

---
+++ src/context/theme.Context.jsx
@@ -0,0 +1,32 @@
+import { createContext, useContext, useState } from "react";
+
+// 1.create context
+const DarkThemeContext = createContext();
+
+// 2.create provider
+const DarkThemeProvider = ({ children }) => {
+  // theme boolean count
+  const [theme, setTheme] = useState(false);
+
+  const toggleDarkTheme = () => {
+    setTheme(!theme);
+  };
+
+  const value = {
+    theme,
+    toggleDarkTheme,
+  };
+
+  return (
+    <DarkThemeContext.Provider value={value}>
+      {children}
+    </DarkThemeContext.Provider>
+  );
+};
+
+// creating custom hook
+const useDarkTheme = () => {
+  return useContext(DarkThemeContext);
+};
+
+export { DarkThemeProvider, useDarkTheme };
