0863c8fa79ad623b8af043a2de4406cdb1455daf
Janarthanan2107 <janarthanan.v2107@gmail.com>
2023-10-22 21:51:56 +0530

    theming bg

--- src/components/navbar/navbar.Component.jsx
+++ src/components/navbar/navbar.Component.jsx
@@ -1,15 +1,42 @@
+// import context
+import { useDarkTheme } from "../../context/theme.Context";
+
 const Navbar = () => {
+  // use our theme context here!
+  const { theme } = useDarkTheme();
   return (
-    <div className="navbar fixed top-0 left-0 w-full shadow-md bg-white z-10">
+    <div
+      className={`navbar fixed top-0 left-0 w-full shadow-md ${
+        theme ? "bg-black" : "bg-white"
+      } z-10 transition duration-500 ease-out hover:ease-in`}
+    >
       <div className="flex justify-between items-center m-5">
-        <div className="logo font-poppins text-[1.5rem] tracking-wide font-bold text-gray-600">
-        <i class="fa-solid fa-code text-blue-500"></i> Janarthanan.
+        <div className={`logo font-poppins text-[1.5rem] tracking-wide font-bold ${theme ? "text-gray-300":"text-gray-600"} `}>
+          <i className="fa-solid fa-code text-blue-500"></i> Janarthanan.
         </div>
         <div className="menu">
-          <ul className="flex gap-7 text-[1.1150rem]">
-            <li className="cursor-pointer hover:text-primary border-b border-b-white hover:border-b-primary">Home</li>
-            <li className="cursor-pointer hover:text-primary border-b border-b-white hover:border-b-primary">Skills</li>
-            <li className="cursor-pointer hover:text-primary border-b border-b-white hover:border-b-primary">Portfolio</li>
+          <ul className="flex gap-7 text-[1.1150rem] font-medium">
+            <li
+              className={`cursor-pointer hover:text-primary hover:-translate-y-[5px] ${
+                theme ? "text-white" : "text-black"
+              }`}
+            >
+              Home
+            </li>
+            <li
+              className={`cursor-pointer hover:text-primary hover:-translate-y-[5px] ${
+                theme ? "text-white" : "text-black"
+              }`}
+            >
+              Skills
+            </li>
+            <li
+              className={`cursor-pointer hover:text-primary hover:-translate-y-[5px] ${
+                theme ? "text-white" : "text-black"
+              }`}
+            >
+              Portfolio
+            </li>
           </ul>
         </div>
         <div className="contact">
